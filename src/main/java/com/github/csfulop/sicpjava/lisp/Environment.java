f4967ddc87033be7fce9802e3315baa92866ff79
Csaba Fülöp <csfulop@users.noreply.github.com>
2019-04-23 20:38:09 +0200

    LISP: extend environment

--- src/main/java/com/github/csfulop/sicpjava/lisp/Environment.java
+++ src/main/java/com/github/csfulop/sicpjava/lisp/Environment.java
@@ -2,11 +2,13 @@ package com.github.csfulop.sicpjava.lisp;
 
 import com.github.csfulop.sicpjava.lisp.exceptions.LispException;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Environment {
     private Map<String, Object> env = new HashMap<>();
+    private Environment enclosing = null;
 
     public void define(String key, Object value) {
         env.put(key, value);
@@ -22,8 +24,27 @@ public class Environment {
     public Object get(String key) {
         Object value = env.get(key);
         if (value == null) {
-            throw new LispException("Unbound variable " + key + "!");
+            if (enclosing == null) {
+                throw new LispException("Unbound variable " + key + "!");
+            } else {
+                return enclosing.get(key);
+            }
         }
         return value;
+    }
+
+    public Environment extend(String[] variables, Object[] values) {
+        Environment extended = new Environment();
+        extended.enclosing = this;
+        if (variables.length < values.length) {
+            throw new LispException("Too many values supplied: " + Arrays.toString(variables) + " -- " + Arrays.toString(values));
+        }
+        if (variables.length > values.length) {
+            throw new LispException("Too many variables supplied: " + Arrays.toString(variables) + " -- " + Arrays.toString(values));
+        }
+        for (int i = 0; i < variables.length; i++) {
+            extended.define(variables[i], values[i]);
+        }
+        return extended;
     }
 }
--- src/test/java/com/github/csfulop/sicpjava/lisp/TestEnvironment.java
+++ src/test/java/com/github/csfulop/sicpjava/lisp/TestEnvironment.java
@@ -63,4 +63,28 @@ public class TestEnvironment {
         //then
         assertThat(exception.getMessage(), is("Unbound variable asdf!"));
     }
+
+    @Test
+    void extendEnvironment() {
+        // given
+        env.define("a", 1);
+        env.define("b", 3);
+        // when
+        Environment e2 = env.extend(new String[]{"a"}, new Object[]{2});
+        // then
+        assertThat(e2.get("a"), is(2));
+        assertThat(e2.get("b"), is(3));
+        assertThrows(LispException.class, () -> e2.get("c"));
+    }
+
+    @Test
+    void extendEnvironmentWithTooManyValues() {
+        LispException exception = assertThrows(LispException.class, () -> env.extend(new String[]{"a"}, new Object[]{2, 3}));
+        assertThat(exception.getMessage(), is("Too many values supplied: [a] -- [2, 3]"));
+    }
+    @Test
+    void extendEnvironmentWithTooManyVariables() {
+        LispException exception = assertThrows(LispException.class, () -> env.extend(new String[]{"a","b"}, new Object[]{2}));
+        assertThat(exception.getMessage(), is("Too many variables supplied: [a, b] -- [2]"));
+    }
 }
